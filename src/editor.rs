a698d983d3e492f63ad961d170a3820c4c326b2f
William Tange <will@tan.ge>
2024-02-10 23:55:49 +0100

    Annoated errors

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,15 @@
 use dioxus::prelude::*;
 use wasm_bindgen::prelude::*;
 
+#[derive(serde::Serialize)]
+struct Annotation {
+    pub row: usize,
+    pub column: usize,
+    pub text: String,
+    #[serde(rename = "type")]
+    pub ty: String,
+}
+
 #[wasm_bindgen]
 extern "C" {
     type EditorInstance;
@@ -10,7 +19,13 @@ extern "C" {
     fn edit(s: &str) -> EditorInstance;
 
     #[wasm_bindgen(method)]
+    fn setValue(this: &EditorSession, val: String);
+
+    #[wasm_bindgen(method)]
     fn getValue(this: &EditorSession) -> String;
+
+    #[wasm_bindgen(method)]
+    fn setAnnotations(this: &EditorSession, annots: JsValue);
 
     #[wasm_bindgen(method)]
     fn getSession(this: &EditorInstance) -> EditorSession;
@@ -21,14 +36,49 @@ extern "C" {
 
 #[component]
 pub fn Editor(cx: Scope) -> Element {
+    use gloo_utils::format::JsValueSerdeExt;
     let editor = use_state(cx, || None);
     use_effect(cx, (editor,), |(editor,)| async move {
         if editor.is_none() {
             let instance: EditorInstance = edit("editor");
+            instance.getSession().setValue(
+                r#"define i32 @main() {
+    %0 = add i32 1, 2
+    ret i32 %0
+}"#
+                .into(),
+            );
             let closure = Closure::new({
                 let session = instance.getSession();
                 move || {
-                    log::info!("{:?}", session.getValue());
+                    use mullvm_parser::PestParser;
+                    let str = session.getValue();
+                    log::info!("{:?}", str);
+                    match mullvm_parser::LLVMParser::parse(mullvm_parser::Rule::module, &str) {
+                        Ok(_) => {
+                            log::info!("ok!");
+                            session.setAnnotations(
+                                JsValue::from_serde(&Vec::<Annotation>::new()).unwrap(),
+                            );
+                        }
+                        Err(e) => {
+                            log::error!("{e}");
+                            match e.line_col {
+                                mullvm_parser::LineColLocation::Pos((l, c))
+                                | mullvm_parser::LineColLocation::Span((l, c), _) => {
+                                    session.setAnnotations(
+                                        JsValue::from_serde(&vec![Annotation {
+                                            row: l - 1,
+                                            column: c - 1,
+                                            text: e.variant.message().into(),
+                                            ty: "error".into(),
+                                        }])
+                                        .unwrap(),
+                                    );
+                                }
+                            }
+                        }
+                    }
                 }
             });
             instance.getSession().on("change", &closure);
