d8460a2525fc997fc9a2b75d13412af3db819c5c
William Tange <will@tan.ge>
2024-02-24 13:52:46 +0100

    not sure what this does

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,57 @@
 #![allow(non_snake_case)]
 
-use std::collections::HashSet;
-
 use dioxus::prelude::*;
-use log::LevelFilter;
 use wasm_bindgen::prelude::*;
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq, serde::Serialize)]
-pub struct Annotation {}
-
-#[wasm_bindgen]
-extern "C" {
-    type EditorInstance;
-    type EditorSession;
-    #[wasm_bindgen(js_namespace = ace)]
-    fn require(s: &str) -> JsValue;
-    #[wasm_bindgen(js_namespace = ace)]
-    fn edit(s: &str) -> EditorInstance;
-    #[wasm_bindgen(method)]
-    fn getSession(this: &EditorInstance) -> EditorSession;
-    #[wasm_bindgen(method)]
-    fn setAnnotations(this: &EditorSession, annots: JsValue);
-    #[wasm_bindgen(method)]
-    fn on(this: &EditorSession, ev: &str, func: &Closure<dyn FnMut()>);
-}
-
 #[component]
-pub fn Editor(annotations: Signal<HashSet<Annotation>>, onchange: EventHandler<String>) -> Element {
-    use gloo_utils::format::JsValueSerdeExt;
+pub fn Editor(state: Signal<()>) -> Element {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
     let mut editor = use_signal(|| None);
     use_effect({
         move || {
-            log::info!("1st effect");
+            log::info!("enter 1st effect");
             if editor.read().is_none() {
-                let instance: EditorInstance = edit("editor");
+                let elem = document.get_element_by_id("editor").unwrap();
                 let closure = Closure::new({
-                    let onchange = onchange.clone();
                     move || {
-                        log::info!("change emitted by ace");
-                        //onchange.call(String::new());
-                        *annotations.write() = HashSet::new();
-                        log::info!("change in ace finished");
+                        log::info!("enter event handler");
+                        *state.write() = ();
+                        log::info!("exit event handler");
                     }
                 });
-                instance.getSession().on("change", &closure);
+                elem.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())
+                    .unwrap();
                 closure.forget();
-                *editor.write() = Some(instance);
+                *editor.write() = Some(elem);
             }
-            log::warn!("1st effect finished");
+            log::warn!("exit 1st effect");
         }
     });
     use_effect(move || {
-        log::info!("2nd effect");
+        log::info!("enter 2nd effect");
         if let Some(editor) = &*editor.read() {
-            let session = editor.getSession();
-            session.setAnnotations(
-                JsValue::from_serde(&annotations.read().iter().collect::<Vec<_>>()).unwrap(),
-            );
-            log::warn!("2nd effect finished");
+            editor.set_inner_html(&format!("{:?}", (&state))); // THIS
         }
+        log::warn!("exit 2nd effect");
     });
     rsx! {
         div {
             id: "editor",
-            class: "h-full",
+            class: "h-full bg-slate-100",
             ""
         }
     }
 }
 
 fn app() -> Element {
-    let mut annotations = use_signal(HashSet::new);
+    let state = use_signal(|| ());
     rsx! {
         div {
             div {
                 class: "h-screen",
                 Editor {
-                    annotations: annotations,
-                    onchange: move |s: String| {
-                        log::info!("change emitted by dx");
-                        *annotations.write() = HashSet::new();
-                        log::info!("change in dx finished");
-                    }
+                    state,
                 }
             }
         }
@@ -89,7 +59,7 @@ fn app() -> Element {
 }
 
 fn main() {
-    dioxus_logger::init(LevelFilter::Trace).expect("failed to init logger");
+    dioxus_logger::init(log::LevelFilter::Trace).expect("failed to init logger");
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default_with_config(
         tracing_wasm::WASMLayerConfigBuilder::default()
